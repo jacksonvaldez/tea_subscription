918164b075e521f53cb3ccf4442208f0941045e6
Jackson Valdez <jacksonvaldez64@gmail.com>
2022-04-19 15:28:17 -0600

    Add routes and controller actions

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+
+      get 'customers/:customer_id/subscriptions', to: 'customers/subscriptions#index'
+      post 'customers/:customer_id/subscriptions/:subscription_id', to: 'customers/subscriptions#create'
+      delete 'customers/:customer_id/subscriptions/:subscription_id', to: 'customers/subscriptions#destroy'
+
+    end
+  end
+
 end
