ce374382733f0f6b6f7efa69fa051bb13e4f44d1
Jackson Valdez <jacksonvaldez64@gmail.com>
2022-04-22 17:52:47 -0600

    Fix bug

--- app/controllers/api/v1/customer_subscriptions_controller.rb
+++ app/controllers/api/v1/customer_subscriptions_controller.rb
@@ -2,14 +2,18 @@ class Api::V1::CustomerSubscriptionsController < ApplicationController
 
   def create # create an association between a customer and subscription (start a subscription)
     if params[:customer_id] && params[:subscription_id]
-      cs = CustomerSubscription.create(customer_id: params[:customer_id], subscription_id: params[:subscription_id])
+      cs = CustomerSubscription.where(customer_id: params[:customer_id], subscription_id: params[:subscription_id]).first
+      cs = CustomerSubscription.create(customer_id: params[:customer_id], subscription_id: params[:subscription_id]) if cs.nil?
+
+      cs.status == 0 ? status = 'active' : status = 'cancelled'
 
       render json: {
         type: 'CustomerSubscription',
         data: {
           id: cs.id,
           customer_id: cs.customer_id,
-          subscription_id: cs.subscription_id
+          subscription_id: cs.subscription_id,
+          status: status
         }
       }
     else
